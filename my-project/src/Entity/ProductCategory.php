1d7f6e202607a0eb6664ea823771f9156b4c8c65
Virtua <m.skalny@wearevirtua.com>
2018-07-31 16:29:17 +0200

    VIMS-28

--- my-project/src/Entity/ProductCategory.php
+++ my-project/src/Entity/ProductCategory.php
@@ -13,6 +13,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProductCategoryRepository")
@@ -49,6 +50,9 @@ class ProductCategory
 
     /**
      * @ORM\Column(type="string")
+     *
+     * @Assert\NotBlank(message="Please, upload the product brochure as a PDF file.")
+     * @Assert\File(mimeTypes={ "image/png" })
      */
     private $image;
 
