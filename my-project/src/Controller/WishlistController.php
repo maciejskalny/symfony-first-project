782d6171de83a16f059cb25e649803e932ed7e88
Virtua <m.skalny@wearevirtua.com>
2018-08-06 16:27:51 +0200

    VIMS-35

--- my-project/src/Controller/WishlistController.php
+++ my-project/src/Controller/WishlistController.php
@@ -5,15 +5,15 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class CartController extends Controller
+class WishlistController extends Controller
 {
     /**
-     * @Route("/cart", name="cart")
+     * @Route("/wishlist", name="wishlist")
      */
     public function index()
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+        return $this->render('wishlist/index.html.twig', [
+            'controller_name' => 'WishlistController',
         ]);
     }
 }
