822c20f54bf4147fcd6cd81df43a398a6c2fb14c
Virtua <m.skalny@wearevirtua.com>
2018-08-08 09:24:48 +0200

    VIMS-39

--- my-project/src/Controller/RegistrationController.php
+++ my-project/src/Controller/RegistrationController.php
@@ -2,18 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class RegistrationController extends Controller
 {
     /**
-     * @Route("/registration", name="registration")
+     * @Route("/register", name="user_registration")
      */
-    public function index()
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
-        return $this->render('registration/index.html.twig', [
-            'controller_name' => 'RegistrationController',
-        ]);
+        $user = new User();
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'You are registred.'
+            );
+
+            return $this->redirectToRoute('welcome');
+
+        }
+
+        return $this->render(
+            'registration/register.html.twig',
+            array('form'=>$form->createView())
+        );
     }
 }
