ad8e8ce3dcb4d046497dd18981e948ad1766a433
Virtua <m.skalny@wearevirtua.com>
2018-08-13 09:53:05 +0200

    VIMS-41, show and edit

--- my-project/src/Controller/ApiProductController.php
+++ my-project/src/Controller/ApiProductController.php
@@ -11,6 +11,8 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -23,7 +25,7 @@ class ApiProductController extends Controller
      * @Route("api/product")
      * @Method("POST")
      */
-    public function newAction(Request $request)
+    public function newProduct(Request $request)
     {
         $nameQuery = $request->query->get('name');
         $descriptionQuery = $request->query->get('description');
@@ -40,6 +42,49 @@ class ApiProductController extends Controller
         $em->flush();
 
         return new Response('New product added', 201);
+    }
+
+    /**
+     * @Route("/api/product/{id}")
+     * @Method("GET")
+     * @param $id
+     * @return Response
+     */
+    public function showProduct($id)
+    {
+        $product = $this->getDoctrine()->getRepository(Product::class)->findOneBy(['id' => $id]);
+
+        $data = [
+            'name' => $product->getName(),
+            'description' => $product->getDescription(),
+            'category' => $product->getCategory(),
+            'created_at' => $product->getAddDate(),
+            'last_modified' => $product->getLastModifiedDate(),
+        ];
+
+        return new Response(json_encode($data));
+    }
+
+    /**
+     * @Route("/api/product/{id}/edit")
+     * @Method("POST")
+     * @param Request $request
+     * @param $id
+     * @return Response
+     */
+    public function editProduct(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $product = $this->getDoctrine()->getRepository(Product::class)->findOneBy(['id' => $id]);
+
+        if($product)
+        {
+            $product->setName($request->get('name'));
+            $product->setDescription($request->get('description'));
+            $em->flush();
+        }
+
+        return new Response('Product updated', 200);
     }
 
 }
